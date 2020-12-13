40ff98b52043fcac50e292cf05ba228c97ee18d5
Jordan Rantucci <jordanrantucci@gmail.com>
2020-12-13 13:39:41 -0500

    adding some pseudocode in logic js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,21 @@
 var startQuiz = document.getElementById("start")
-var submitInitials = document.getElementById("submit") //Adding the element selectors
+var submitInitials = document.getElementById("submit") //defining variables
 
 
 
 
 var totalSeconds = 0;
 var secondsElapsed = 0;
+
+// I have to create a countdown timer that starts when startQuiz button is clicked
+// The timer has to stat at 90 seconds and if a wrong answer is selected 10 seconds are subtracted
+// I have to hide the Coding quiz challenge and paragraph when startQuiz button is clicked
+// On click I have to have question 1 appear with a list of choices a,b,c,d and have 1 be the correct response
+// When correct response is clicked if it is correct move on the next question if it is wrong 10 seconds subtracted from timer
+// 4 additional questions will have to be written 
+// Once quiz is complete the end-screen will appear
+// I will have to create a box that you can enter initials and it will store the info locally
+
 
 
 
