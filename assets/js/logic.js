3e8c8208df63829a8203b1e5ded0c261aa507276
Jordan Rantucci <jordanrantucci@gmail.com>
2020-12-13 13:19:43 -0500

    starting to add variables and added event listener

---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+var startQuiz = document.getElementById("start")
+var submitInitials = document.getElementById("submit") //Adding the element selectors
+
+
+
+
+var totalSeconds = 0;
+var secondsElapsed = 0;
+
+
+
+
+
+
+
+
+
+addEventListener("click", function() {} )
