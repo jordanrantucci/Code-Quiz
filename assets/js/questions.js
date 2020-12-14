ea921480f473938a9417063c1c0113a157f513a6
Jordan Rantucci <jordanrantucci@gmail.com>
2020-12-13 23:29:42 -0500

    trying to figure out how to generate multiple questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -7,19 +7,35 @@ var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 
+
+//li1.SetAttribute(correct, "correct")
+
+
 li1.textContent = "Chicken Fingers";
 li2.textContent = "Pizza";
 li3.textContent = "Burgers";
 li4.textContent = "Sushi";
 
 
+startButton.addEventListener("click", questionOne);
+
 
 function questionOne(){
 document.getElementById("question-title").innerHTML = "Question 1: What is an array?";
-document.getElementById("choices").innerHTML = (listEl.appendChild(li1), listEl.appendChild(li2), listEl.appendChild(li3), listEl.appendChild(li4))
+document.getElementById("choices").appendChild(listEl);
+document.getElementById("choices").appendChild(li1); //I would like to say correct for this response like adding an .onclick = correct
+document.getElementById("choices").appendChild(li2);
+document.getElementById("choices").appendChild(li3);
+document.getElementById("choices").appendChild(li4);
+
 }
 
+//li1.SetAttribute("choices", "specialListItem")
 
+li1.addEventListener("click", questionTwo)
 
+function questionTwo(){
+    document.getElementById("question-title").innerHTML = "Question 2: What is an array?";
+    document.getElementById("choices").removeChild(listEl);
+}
 
-startButton.addEventListener("click", questionOne);
